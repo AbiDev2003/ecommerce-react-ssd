a25438483b122696aa38accddf39381a14476bb6
AbiDev2003 <2003abinashdash@gmail.com>
2026-05-09 01:47:49 +0530

    checkout page integration testing 02

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -40,16 +40,16 @@ function CartItemDetails({ cartItem, loadCart }) {
 
   return (
     <>
-      <img className="product-image" src={cartItem.product.image} />
+      <img className="product-image" src={cartItem.product.image} data-testid="cart-item-image"/>
 
       <div className="cart-item-details">
-        <div className="product-name">{cartItem.product.name}</div>
-        <div className="product-price">
+        <div className="product-name" data-testid="cart-item-name">{cartItem.product.name}</div>
+        <div className="product-price" data-testid="cart-item-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
         <div className="product-quantity">
-          <span>
-            Quantity:
+          <span data-testid="cart-item-quantity">
+            Quantity:{" "}
             {isUpdatingQuantity ? (
               <input
                 type="text"
@@ -70,6 +70,7 @@ function CartItemDetails({ cartItem, loadCart }) {
           </span>
           <span
             className="delete-quantity-link link-primary"
+            data-testid="cart-item-delete-quantity-link"
             onClick={deleteCartItem}
           >
             Delete
